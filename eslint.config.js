40521dbf9c6beefbd2809a7a28f2c9894cfd0073
Michael S. <siniq90@gmail.com>
2026-06-19 20:30:57 +0200

    Add ESLint flat config
    Lints resources/js with @eslint/js, typescript-eslint, react,
    react-hooks, react-refresh. Defers formatting rules to Prettier via
    eslint-config-prettier. Adds 'lint' script to package.json.

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  { ignores: ['public', 'dist', 'node_modules'] },
+  {
+    files: ['resources/js/**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
+      prettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: { window: 'readonly', document: 'readonly' },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
